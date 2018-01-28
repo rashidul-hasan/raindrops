b1538bb42271aa530f1f720f69500b3f9858cf03
Rashidul Hasan <rashidul69@gmail.com>
2018-01-28 19:56:04 +0600

    Replace data transformer class for modify datatable's data.
    Move data transforming logic in Data trait so it's easier to override them from the controller.

--- src/Crud/Data.php
+++ src/Crud/Data.php
@@ -8,9 +8,14 @@
 
 namespace Rashidul\RainDrops\Crud;
 
+use Rashidul\RainDrops\Model\ModelHelper;
+use Rashidul\RainDrops\Table\Helper;
+
 
 trait Data
 {
+
+    protected $helper;
 
     /**
      * handle datatable server side
@@ -20,8 +25,12 @@ trait Data
 
         $tableActions = $this->crudAction->getTableActions();
         $this->dataTableQuery = $this->model->select();
-        $this->dataTableObject = $this->dataTable->eloquent($this->dataTableQuery)
-            ->setTransformer(new $this->dataTransformer($tableActions));
+        $this->dataTableObject = $this->dataTable->eloquent($this->dataTableQuery);
+        $this->helper = new Helper();
+
+        $this->editColumns();
+
+        $this->addActionColumn($tableActions);
 
         // let user modify the query builder object to
         // further customize the data to be feed to the
@@ -33,6 +42,38 @@ trait Data
 
         return $this->dataTableObject->make(true);
 
+    }
+
+    protected function editColumns()
+    {
+        $fields = ModelHelper::getIndexFields( $this->model );
+
+        foreach ($fields as $field => $options)
+        {
+            $dataType = $this->helper->getDataType($options);
+
+            $this->dataTableObject->editColumn($field, function ($item) use($field, $options,$dataType){
+                return $this->helper->get($item, $field, $options, $dataType);
+            });
+        }
+    }
+
+    protected function addActionColumn($tableActions)
+    {
+        $this->dataTableObject->editColumn('action', function ($item) use ($tableActions){
+
+            $crudAction = new CrudAction($item);
+
+            if (method_exists($this, 'getActions'))
+            {
+                $actions = $this->getActions();
+            }
+            else
+            {
+                $actions = $tableActions;
+            }
+            return $crudAction->render($crudAction->replaceRoutesInActions($actions));
+        });
     }
 
 }
