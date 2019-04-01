350741729f5edfaa3c239e1d1c0cfd97481ea4c6
Rashidul Hasan <rashidul69@gmail.com>
2019-04-01 19:38:54 +0600

    Remove  operator which was removed from Laravel 5.8

--- views/form/horizontal/basic.blade.php
+++ views/form/horizontal/basic.blade.php
@@ -1,7 +1,7 @@
-<div class="form-group {{ $error_class or '' }}">
-    <label for="{{ $id or '' }}" class="col-sm-3 control-label">{!! $label or '' !!}</label>
+<div class="form-group {{ $error_class ?? '' }}">
+    <label for="{{ $id ?? '' }}" class="col-sm-3 control-label">{!! $label ?? '' !!}</label>
     <div class="col-sm-6">
         {!! $field !!}
-        <span class="help-block">{{ $error_text or '' }}</span>
+        <span class="help-block">{{ $error_text ?? '' }}</span>
     </div>
 </div>
--- views/form/horizontal/checkbox.blade.php
+++ views/form/horizontal/checkbox.blade.php
@@ -1,9 +1,9 @@
-<div class="form-group {{ $error_class or '' }}">
+<div class="form-group {{ $error_class ?? '' }}">
     <div class="col-sm-offset-3 col-sm-6">
         <div class="checkbox">
-            <label for="{{ $id or '' }}">
-                {!! $field !!} {!! $label or '' !!}
-                <span class="help-block">{{ $error_text or '' }}</span>
+            <label for="{{ $id ?? '' }}">
+                {!! $field !!} {!! $label ?? '' !!}
+                <span class="help-block">{{ $error_text ?? '' }}</span>
             </label>
         </div>
     </div>
